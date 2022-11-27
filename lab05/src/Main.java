6123907f40269c94a6f8c7946ec356a73de256cd
Romanko Mikhail Pavlovich <me@blankhex.com>
2022-11-27 19:18:54 +0300

    Fix issues with fifth lab.

--- lab05/src/Main.java
+++ lab05/src/Main.java
@@ -1,20 +1,33 @@
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Collection<Integer> integerCollection = Arrays.asList(1, 1, 2, 3, 4, 5, 6);
-        Collection<String> stringCollection = Arrays.asList("Input", "Output", "Outperform", "Intrade", "Outrun");
-        Collection<String> smallStringCollection = Arrays.asList("Input");
-        Collection<String> mapStringCollection = Arrays.asList("Input", "Output");
+        List<Integer> integerList = Arrays.asList(1, 1, 2, 3, 4, 5, 6);
+        System.out.println("getAverage: " + Streamable.getAverage(integerList));
+        System.out.println("getSquare: " + Streamable.getSquare(integerList));
 
-        System.out.println("getAverage: " + Streamable.getAverage(integerCollection));
-        System.out.println("getSquare: " + Streamable.getSquare(integerCollection));
-        System.out.println("getSum: " + Streamable.getSum(integerCollection));
-        System.out.println("getUpperString: " + Streamable.getUpperString(stringCollection));
+        Integer[] integerArray = new Integer[]{1, 1, 2, 3, 4, 5, 6};
+        System.out.println("getSum: " + Streamable.getSum(integerArray));
+
+        List<String> stringList = Arrays.asList("Input", "Output", "Outperform", "Intrade", "Outrun");
+        System.out.println("getUpperString: " + Streamable.getUpperString(stringList));
+
+        Collection<String> stringCollection = Arrays.asList("Input", "Output", "Outperform", "Intrade", "Outrun");
         System.out.println("getSorted: " + Streamable.getSorted(stringCollection, "Out"));
         System.out.println("findLast: " + Streamable.findLast(stringCollection));
+
+        Collection<String> smallStringCollection = Arrays.asList("Input");
         System.out.println("findLast: " + Streamable.findLast(smallStringCollection));
-        System.out.println("convertToMap: " + Streamable.convertToMap(mapStringCollection));
+        try {
+            Collection<String> emptyStringCollection = Arrays.asList();
+            System.out.println("findLast: " + Streamable.findLast(emptyStringCollection));
+        } catch(Exception e) {
+            System.out.println("findLast: throwed exception");
+        }
+
+        List<String> mapStringList = Arrays.asList("Input", "Output");
+        System.out.println("convertToMap: " + Streamable.convertToMap(mapStringList));
     }
 }
